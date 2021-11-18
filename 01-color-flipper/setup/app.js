0225e2b54977c09b64ebbf5e3e66b864893bf438
Adrien <adriencyr@ymail.com>
2021-11-18 09:57:11 -0800

    Revert "DOM Assignment"
    This reverts commit 10e0cad5728fba28cf1b02472db7ceecbec7b7ce.

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,14 +1,1 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
-
-
-function randomNumber() {
-    return Math.floor(Math.random() * colors.length)
-}
-
-btn.addEventListener("click", function () {
-    const index = randomNumber();
-    document.body.style.backgroundColor = colors[index];
-    color.textContent = colors[index];
-})
